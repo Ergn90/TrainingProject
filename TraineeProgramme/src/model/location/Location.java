c2819b53ac8bd5d32bec9a7fe34065a8ed56d366
dtolstorukova <dina.tolstorukova@trivadis.com>
2019-04-24 22:29:12 +0200

    skala and location beans

--- TraineeProgramme/src/model/location/Location.java
+++ TraineeProgramme/src/model/location/Location.java
@@ -1,4 +1,33 @@
 package model.location;
 
 public class Location {
+
+    private int locationId;
+    private String locationName;
+
+    public Location() {
+    }
+
+    public Location(int locationId, String locationName) {
+        this.locationId = locationId;
+        this.locationName = locationName;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+
 }
--- TraineeProgramme/src/model/skala/Skala.java
+++ TraineeProgramme/src/model/skala/Skala.java
@@ -1,4 +1,30 @@
 package model.skala;
 
 public class Skala {
+    private int skalaId;
+    private String skalaName;
+
+    public Skala() {
+    }
+
+    public Skala(int skalaId, String skalaName) {
+        this.skalaId = skalaId;
+        this.skalaName = skalaName;
+    }
+
+    public int getSkalaId() {
+        return skalaId;
+    }
+
+    public void setSkalaId(int skalaId) {
+        this.skalaId = skalaId;
+    }
+
+    public String getSkalaName() {
+        return skalaName;
+    }
+
+    public void setSkalaName(String skalaName) {
+        this.skalaName = skalaName;
+    }
 }
