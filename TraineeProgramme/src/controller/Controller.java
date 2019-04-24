caff5e89d195f1006354890dd50485d77dde3ff0
andradaserban1 <andradaserban3636@gmail.com>
2019-04-24 17:47:57 +0200

    init trainee view

--- TraineeProgramme/src/controller/Controller.java
+++ TraineeProgramme/src/controller/Controller.java
@@ -1,4 +1,35 @@
 package controller;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    private Label label;
+
+    @FXML
+    public  void handleAction(){
+        label.setText("OK Button pressed");
+    }
+
+    @FXML
+    private Button ok;
+
+    @FXML
+    private Button cancel;
+
+    @FXML
+    private Button help;
+
+
 }
