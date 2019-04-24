b140d9de4912af2b91c5760a3f441988877a68e1
Ergn90 <g.michel90@gmail.com>
2019-04-24 17:25:57 +0200

    Dao interface

--- TraineeProgramme/src/model/location/LocationDao.java
+++ TraineeProgramme/src/model/location/LocationDao.java
@@ -1,4 +1,11 @@
 package model.location;
 
+import java.util.Set;
+
 public interface LocationDao {
+    Location getLocation(int id);
+    Set<Location> getAllLocation();
+    boolean insertLocation(Location location);
+    boolean updateLocation(Location courses);
+    boolean deleteLocation(int id);
 }
