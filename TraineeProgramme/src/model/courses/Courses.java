ba12def993b66f2bbaefada477a35fa4de03c3e9
dtolstorukova <dina.tolstorukova@trivadis.com>
2019-04-24 17:59:55 +0200

    trainee and courses beans

--- TraineeProgramme/src/model/courses/Courses.java
+++ TraineeProgramme/src/model/courses/Courses.java
@@ -1,4 +1,44 @@
 package model.courses;
 
+import java.util.Date;
+
 public class Courses {
+    private int courseID;
+    private Date courseYear;
+    private int courseName;
+    private String courseRoom;
+
+    public Courses(int courseID, Date courseYear, int courseName, String courseRoom) {
+        this.courseID = courseID;
+        this.courseYear = courseYear;
+        this.courseName = courseName;
+        this.courseRoom = courseRoom;
+    }
+
+    public Courses() {
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public int getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(int courseName) {
+        this.courseName = courseName;
+    }
+
+    public Date getCourseYear() {
+        return courseYear;
+    }
+
+    public void setCourseYear(Date courseYear) {
+        this.courseYear = courseYear;
+    }
 }
--- TraineeProgramme/src/model/trainee/Trainee.java
+++ TraineeProgramme/src/model/trainee/Trainee.java
@@ -1,4 +1,93 @@
 package model.trainee;
 
+import java.util.Date;
+
 public class Trainee {
+    private int traineeID;
+    private String lastName;
+    private String firstName;
+    private Date birthday;
+    private String address;
+    private String school;
+    private String email;
+    private int locationId;
+
+    Trainee() {
+    }
+
+    Trainee(int traineeID, String lastName, String firstName, Date birthday, String address, String school, String email, int locationId) {
+        this.traineeID = traineeID;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.birthday = birthday;
+        this.address = address;
+        this.school = school;
+        this.email = email;
+        this.locationId = locationId;
+    }
+
+    public Integer getTraineeID() {
+        return traineeID;
+    }
+
+    public void setTraineeID(int traineeID) {
+        this.traineeID = traineeID;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
 }
+
