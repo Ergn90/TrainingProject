3455a3883d8666f167d3746877f499489b1be7de
andradaserban1 <andradaserban3636@gmail.com>
2019-04-25 17:12:37 +0200

    Merge branch 'master' of github.com:Ergn90/TrainingProject
    # Conflicts:
    #	TraineeProgramme/src/view/CourseForm.fxml

---
+++ TraineeProgramme/src/model/course/CourseDaoImpl.java
@@ -0,0 +1,153 @@
+package model.course;
+
+import model.ConnectionFactory;
+
+import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CourseDaoImpl implements CourseDao {
+
+    private Course extractCoursesFromResult(ResultSet rs) throws SQLException {
+        return new Course(rs.getInt("CourseID"),
+                rs.getDate("CourseDate"),
+                rs.getString("CourseName"),
+                rs.getString("CourseRoom"),
+                rs.getString("CourseDescription"));
+    }
+
+    @Override
+    public Course getCourse(int id) {
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+
+            Statement stmt = connection.createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Courses where CourseID =" + id);
+
+            if (rs.next()) {
+                return extractCoursesFromResult(rs);
+            }
+
+        } catch (SQLException ex) {
+
+            ex.printStackTrace();
+
+        }
+        return null;
+    }
+
+    @Override
+    public Set<Course> getAllCourses() {
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Courses");
+            Set<Course> coursesList = new HashSet<>();
+            while (resultSet.next()) {
+                Course course = extractCoursesFromResult(resultSet);
+                coursesList.add(course);
+            }
+            return coursesList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    @Override
+    public Set<Course> getCoursesByNameAndDate(String name, java.util.Date date) {
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Courses where CourseName =" + name + " and CourseDate=" + new java.sql.Date(date.getTime()));
+            Set<Course> coursesSet = new HashSet();
+            while (rs.next()) {
+                Course course = extractCoursesFromResult(rs);
+                coursesSet.add(course);
+            }
+            return coursesSet;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean insertCourse(Course course) {
+        Connection conn = ConnectionFactory.getConnection();
+        try {
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO Courses VALUES (NULL,?, ?, ?, ?)");
+            ps.setDate(1, new java.sql.Date(course.getStartDate().getTime()));
+            ps.setString(2, course.getCourseName());
+            ps.setString(3, course.getCourseDescription());
+            ps.setString(4, course.getCourseRoom());
+            int i = ps.executeUpdate();
+            if (i == 1) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateCourse(Course course) {
+        try {
+            Connection conn = ConnectionFactory.getConnection();
+            PreparedStatement ps = conn.prepareStatement("UPDATE Courses SET CourseDate=?, CourseName=?, CourseDescription=? , CourseRoom=? WHERE CourseID=?");
+
+            ps.setDate(2, new java.sql.Date(course.getStartDate().getTime()));
+            ps.setString(3, course.getCourseName());
+            ps.setString(4, course.getCourseDescription());
+            ps.setString(5, course.getCourseRoom());
+            int i = ps.executeUpdate();
+            if (i == 1) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteCourse(int id) {
+        Connection conn = ConnectionFactory.getConnection();
+        try {
+            Statement stmt = conn.createStatement();
+            int i = stmt.executeUpdate("DELETE FROM Courses WHERE CourseID=" + id);
+            if (i == 1) {
+                return true;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- TraineeProgramme/src/model/enrolledTrainees/EnrolledTraineesDaoImpl.java
+++ TraineeProgramme/src/model/enrolledTrainees/EnrolledTraineesDaoImpl.java
@@ -1,129 +1,324 @@
 package model.enrolledTrainees;
 
 import model.ConnectionFactory;
-import model.courses.Courses;
+import model.course.Course;
+import model.location.Location;
 import model.skala.Skala;
 import model.trainee.Trainee;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EnrolledTraineesDaoImpl implements EnrolledTraineesDao {
-    private static final String SELECTALLCOURSESBYTRAINEESID = "SELECT courses.CourseID,CourseName,CourseYear,CourseRoom,CourseDescription " +
-            "FROM courses,trainee,enrolled_trainees  " +
-            "where   enrolled_trainees.TraineeID=";
-    private static final String SELECTALLSKALABYTRAINEES = "SELECT skala.SkalaId,SkalaName " +
-            "FROM courses,trainee,enrolled_trainees,skala  " +
+    private static final String SELECTCOURSESBYTRAINEESID = "SELECT courses.CourseID,CourseName,CourseDate,CourseRoom,CourseDescription from courses " +
+            "inner join enrolled_trainees on  enrolled_trainees.CourseID=courses.CourseID " +
+            "where enrolled_trainees.TraineeID=";
+    private static final String SELECTSKALABYTRAINEESID = "SELECT skala.SkalaId,SkalaName " +
+            "from skala inner join enrolled_trainees on  enrolled_trainees.SkalaID=skala.SkalaId " +
+            "where enrolled_trainees.TraineeID=";
+
+    private static final String SELECTSKALABYCOURSEID = "SELECT skala.SkalaId,SkalaName " +
+            "from skala inner join enrolled_trainees on  enrolled_trainees.SkalaID=skala.SkalaId " +
+            "where enrolled_trainees.CourseID=";
+    private static final String SELECTTRAINEEBYCOURSEID = "SELECT trainee.TraineeID,LastName,    FirstName,    Birthday,    Address,    School,Email,    trainee.LocationId,LocationName " +
+            "from trainee inner join enrolled_trainees on enrolled_trainees.TraineeID=trainee.TraineeID " +
+            "inner join location on trainee.LocationId=location.LocationId " +
+            "where enrolled_trainees.CourseID=";
+    private static final String SELECTTRAINEEBYSKALAID = "SELECT trainee.TraineeID,LastName,    FirstName,    Birthday,    Address,    School,Email,    trainee.LocationId,LocationName " +
+            "from trainee inner join enrolled_trainees on enrolled_trainees.TraineeID=trainee.TraineeID " +
+            "inner join location on trainee.LocationId=location.LocationId " +
             "where  enrolled_trainees.SkalaID=";
+    private static final String SELECTCOURSESBYSKALAID = "SELECT courses.CourseID,CourseName,CourseDate,CourseRoom,CourseDescription from courses " +
+            "inner join enrolled_trainees on  enrolled_trainees.CourseID=courses.CourseID " +
+            "where enrolled_trainees.SkalaID=";
+    private static final String ANDTRAINEEID = " and enrolled_trainees.TraineeID=";
+    private static final String ANDCOURSEID = " and enrolled_trainees.CourseID=";
+    private static final String ANDSKALAEID = " and enrolled_trainees.SkalaID=";
 
-    @Override
-    public List<Courses> getAllCoursesByTrainees(Trainee trainee) {
-        Connection connection= ConnectionFactory.getConnection();
-
-        try {
-            Statement statement =connection.createStatement();
-            ResultSet resultSet=statement.executeQuery(SELECTALLCOURSESBYTRAINEESID+trainee.getTraineeID());
-            List<Courses> coursesList=new ArrayList<>();
-            while(resultSet.next()){
-                Courses  courses=extractCourseFromTrainees(resultSet);
-                coursesList.add(courses);
-            }
-            return  coursesList;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private Courses extractCourseFromTrainees(ResultSet resultSet) throws SQLException {
-        Courses courses=new Courses();
-        courses.setCourseID(resultSet.getInt("CourseID"));
-        courses.setCourseName(resultSet.getInt("CourseName"));
-        courses.setCourseYear(resultSet.getDate("CourseYear"));
-        //courses.setCourseDescription(resultSet.getString("CourseDescription"));
-        return  courses;
-
-    }
-
-    @Override
-    public List<Skala> getAllSkalaByTraines(Trainee trainee) {
-
-        Connection connection= ConnectionFactory.getConnection();
-
-        try {
-            Statement statement =connection.createStatement();
-            ResultSet resultSet=statement.executeQuery(SELECTALLSKALABYTRAINEES+trainee.getTraineeID());
-            List<Skala> skalaList=new ArrayList<>();
-            while(resultSet.next()){
-                Skala  skala=extractSkalaFromTraines(resultSet);
-                skalaList.add(skala);
-            }
-            return  skalaList;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private Skala extractSkalaFromTraines(ResultSet resultSet) throws SQLException {
-        Skala skala=new Skala();
+    private Skala extractSkalaFromResult(ResultSet resultSet) throws SQLException {
+        Skala skala = new Skala();
         skala.setSkalaId(resultSet.getInt("SkalaId"));
         skala.setSkalaName(resultSet.getString("SkalaName"));
-       
-        return  skala;
+
+        return skala;
+    }
+
+    private Course extractCourseFromResult(ResultSet resultSet) throws SQLException {
+        Course course = new Course();
+        course.setCourseID(resultSet.getInt("CourseID"));
+        course.setCourseName(resultSet.getString("CourseName"));
+        course.setStartDate(resultSet.getDate("CourseDate"));
+        course.setCourseDescription(resultSet.getString("CourseDescription"));
+        return course;
+
+    }
+
+    private Trainee extractTraineeFromResult(ResultSet resultSet) throws SQLException {
+        Trainee trainee = new Trainee();
+        trainee.setTraineeID(resultSet.getInt("TraineeID"));
+        trainee.setAddress(resultSet.getString("LastName"));
+        trainee.setFirstName(resultSet.getString("FirstName"));
+        trainee.setBirthday(resultSet.getDate("Birthday"));
+        trainee.setAddress(resultSet.getString("Address"));
+        trainee.setEmail(resultSet.getString("Email"));
+        Location location = new Location();
+        location.setLocationId(resultSet.getInt("LocationID"));
+        location.setLocationName(resultSet.getString("LocationName"));
+        trainee.setLocation(location);
+        return trainee;
     }
 
     @Override
-    public List<Trainee> getAllTraineesByCourses(Courses courses) {
+    public List<Course> getAllCoursesByTrainee(Trainee trainee) {
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SELECTCOURSESBYTRAINEESID + trainee.getTraineeID());
+            List<Course> coursesList = new ArrayList<>();
+            while (resultSet.next()) {
+                Course course = extractCourseFromResult(resultSet);
+                coursesList.add(course);
+            }
+            return coursesList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
+
     @Override
-    public List<Skala> getAllSkalaByCourses(Courses courses) {
+    public List<Skala> getAllSkalaByTrainee(Trainee trainee) {
+
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SELECTSKALABYTRAINEESID + trainee.getTraineeID());
+            List<Skala> skalaList = new ArrayList<>();
+            while (resultSet.next()) {
+                Skala skala = extractSkalaFromResult(resultSet);
+                skalaList.add(skala);
+            }
+            return skalaList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    @Override
+    public List<Trainee> getAllTraineesByCourse(Course course) {
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SELECTTRAINEEBYCOURSEID + course.getCourseID());
+            List<Trainee> traineeList = new ArrayList<>();
+            while (resultSet.next()) {
+                Trainee trainee = extractTraineeFromResult(resultSet);
+                traineeList.add(trainee);
+            }
+            return traineeList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    @Override
+    public List<Skala> getAllSkalaByCourse(Course course) {
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SELECTSKALABYCOURSEID + course.getCourseID());
+            List<Skala> skalaList = new ArrayList<>();
+            while (resultSet.next()) {
+                Skala skala = extractSkalaFromResult(resultSet);
+                skalaList.add(skala);
+            }
+            return skalaList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public List<Trainee> getAllTraineesBySkala(Skala skala) {
+        Connection connection = ConnectionFactory.getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SELECTTRAINEEBYSKALAID + skala.getSkalaId());
+            List<Trainee> traineeList = new ArrayList<>();
+            while (resultSet.next()) {
+                Trainee trainee = extractTraineeFromResult(resultSet);
+                traineeList.add(trainee);
+            }
+            return traineeList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
-    public List<Courses> getAllCoursesBySkala(Skala skala) {
+    public List<Course> getAllCoursesBySkala(Skala skala) {
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SELECTCOURSESBYSKALAID + skala.getSkalaId());
+            List<Course> coursesList = new ArrayList<>();
+            while (resultSet.next()) {
+                Course course = extractCourseFromResult(resultSet);
+                coursesList.add(course);
+            }
+            return coursesList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Skala getSkala(int traineeID, int coursesID) {
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+
+            Statement stmt = connection.createStatement();
+
+            ResultSet rs = stmt.executeQuery(SELECTSKALABYCOURSEID + coursesID + ANDTRAINEEID + traineeID);
+
+            if (rs.next()) {
+                return extractSkalaFromResult(rs);
+
+            }
+
+        } catch (SQLException ex) {
+
+            ex.printStackTrace();
+
+        }
+
         return null;
+
     }
 
     @Override
-    public Courses getCourse(int traineeID, int skalaID) {
+    public Course getCourse(int traineeID, int skalaID) {
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+
+            Statement stmt = connection.createStatement();
+
+            ResultSet rs = stmt.executeQuery(SELECTCOURSESBYTRAINEESID + traineeID + ANDSKALAEID + skalaID);
+
+            if (rs.next()) {
+                return extractCourseFromResult(rs);
+
+            }
+
+        } catch (SQLException ex) {
+
+            ex.printStackTrace();
+
+        }
+
         return null;
     }
 
     @Override
     public Trainee getTrainee(int coursesID, int skalaID) {
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+
+            Statement stmt = connection.createStatement();
+
+            ResultSet rs = stmt.executeQuery(SELECTTRAINEEBYCOURSEID + coursesID + ANDSKALAEID + skalaID);
+
+            if (rs.next()) {
+                return extractTraineeFromResult(rs);
+
+            }
+
+        } catch (SQLException ex) {
+
+            ex.printStackTrace();
+
+        }
+
         return null;
     }
 
     @Override
-    public boolean insertEnrolledTraines(Courses courses, Skala skala, Trainee trainee) {
+    public boolean insertEnrolledTraines(Course course, Skala skala, Trainee trainee) {
+
+
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO enrolled_trainees VALUES (?, ?, ?)");
+
+            ps.setInt(1, trainee.getTraineeID());
+
+            ps.setInt(2, course.getCourseID());
+
+            ps.setInt(3, skala.getSkalaId());
+
+            int i = ps.executeUpdate();
+
+            if (i == 1) {
+
+                return true;
+
+            }
+
+        } catch (SQLException ex) {
+
+            ex.printStackTrace();
+
+        }
+
         return false;
+
     }
 
     @Override
-    public boolean updateEnrolledTraines(Courses courses, Skala skala, Trainee trainee) {
-        return false;
-    }
+    public boolean deletEnrolledTraines(int traineeID, int skalaID, int coursesID) {
 
-    @Override
-    public boolean updateEnrolledTraines(int traineeID, int skalaID, int coursesID) {
+        Connection connection = ConnectionFactory.getConnection();
+
+        try {
+
+            PreparedStatement ps = connection.prepareStatement("DELETE FROM enrolled_trainees WHERE traineeID=?, CourseID=?, SkalaId=?");
+
+            ps.setInt(1, traineeID);
+            ps.setInt(2, coursesID);
+            ps.setInt(3, skalaID);
+
+            int i = ps.executeUpdate();
+            if(i == 1) {
+
+                return true;
+
+            }
+
+        } catch (SQLException ex) {
+
+            ex.printStackTrace();
+
+        }
+
         return false;
     }
 }
+
