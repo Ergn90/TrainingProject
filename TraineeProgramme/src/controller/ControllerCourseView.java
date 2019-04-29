df036a530b1249d4964a49b4f00be4107f899ee7
andradaserban1 <andradaserban3636@gmail.com>
2019-04-29 16:36:00 +0200

    update und delete courses

--- TraineeProgramme/src/controller/ControllerCourseView.java
+++ TraineeProgramme/src/controller/ControllerCourseView.java
@@ -1,9 +1,6 @@
 package controller;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
@@ -14,9 +11,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 import model.course.Course;
 import model.course.CourseDao;
 import model.course.CourseDaoImpl;
+import model.enrolledTrainees.EnrolledTraineesDao;
+import model.enrolledTrainees.EnrolledTraineesDaoImpl;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -51,12 +52,71 @@ public class ControllerCourseView implements Initializable {
     @FXML
     Button backFromCourseViewToTraineeView;
     @FXML
-    TableView coursesTable;
+    TableView<Course> coursesTable;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        getCourseInfo();
+
+        CourseDao courseDao = refreshCourseInfo();
         searchForCourse();
+        addDeleteButtonToTable(courseDao);
+        addUpdateButtonToTable(courseDao);
+    }
+
+
+    public CourseDao refreshCourseInfo() {
+        CourseDao courseDao = new CourseDaoImpl();
+        Set<Course> courses = courseDao.getAllCourses();
+        coursesTable.setItems(FXCollections.observableArrayList(courses));
+
+        courseID.setCellValueFactory(new PropertyValueFactory<>("CourseID"));
+        courseName.setCellValueFactory(new PropertyValueFactory<>("CourseName"));
+        courseRoom.setCellValueFactory(new PropertyValueFactory<>("CourseRoom"));
+        courseDescriptionC.setCellValueFactory(new PropertyValueFactory<>("CourseDescription"));
+        startDate.setCellValueFactory(new PropertyValueFactory<>("startDate"));
+        endDate.setCellValueFactory(new PropertyValueFactory<>("endDate"));
+
+        return courseDao;
+    }
+
+    private void addDeleteButtonToTable(CourseDao courseDao) {
+        TableColumn<Course, Void> colBtn = new TableColumn("Delete Course");
+
+        Callback<TableColumn<Course, Void>, TableCell<Course, Void>> cellFactory = new Callback<TableColumn<Course, Void>, TableCell<Course, Void>>() {
+            @Override
+            public TableCell<Course, Void> call(final TableColumn<Course, Void> param) {
+                final TableCell<Course, Void> cell = new TableCell<Course, Void>() {
+
+                    private final Button btn = new Button("Delete");
+
+                    {
+                        btn.setOnAction((ActionEvent event) -> {
+                            EnrolledTraineesDao enrolledCoursesDao = new EnrolledTraineesDaoImpl();
+                            Course course = (Course) coursesTable.getItems().get(getIndex());
+                            boolean deleteEnrolled = enrolledCoursesDao.deleteEnrolledTraineesByCourse(course.getCourseID());
+                            boolean deleteCourse = courseDao.deleteCourse(course.getCourseID());
+                            refreshCourseInfo();
+
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+
+        colBtn.setCellFactory(cellFactory);
+
+        coursesTable.getColumns().add(colBtn);
     }
 
     private void searchForCourse() {
@@ -66,23 +126,23 @@ public class ControllerCourseView implements Initializable {
         FilteredList<Course> filteredData = new FilteredList<>(coursesList, c -> true);
 
         searchFielCourses.textProperty().addListener((observable, oldValue, newValue) -> {
-             filteredData.setPredicate(course -> {
-                 // If filter text is empty, display all persons.
-                 if (newValue == null || newValue.isEmpty()) {
-                     return true;
-                 }
+            filteredData.setPredicate(course -> {
+                // If filter text is empty, display all persons.
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
 
-                 // Compare first name and last name of every person with filter text.
-                 String lowerCaseFilter = newValue.toLowerCase();
+                // Compare first name and last name of every person with filter text.
+                String lowerCaseFilter = newValue.toLowerCase();
 
-                 if (course.getCourseRoom().toLowerCase().contains(lowerCaseFilter)) {
-                     return true;
-                 } /*else if (course..toLowerCase().contains(lowerCaseFilter)) {
+                if (course.getCourseRoom().toLowerCase().contains(lowerCaseFilter)) {
+                    return true;
+                } /*else if (course..toLowerCase().contains(lowerCaseFilter)) {
                      return true;*/
-                 //}
-                 return false; // Does not match.
-             });
-         });
+                //}
+                return false; // Does not match.
+            });
+        });
 
         // 3. Wrap the FilteredList in a SortedList.
         SortedList<Course> sortedData = new SortedList<>(filteredData);
@@ -94,27 +154,6 @@ public class ControllerCourseView implements Initializable {
         coursesTable.setItems(sortedData);
     }
 
-
-    public void getCourseInfo() {
-        CourseDao courseDao = refreshCourseInfo();
-    }
-
-    public CourseDao refreshCourseInfo() {
-
-        CourseDao courseDao = new CourseDaoImpl();
-        Set<Course> courses = courseDao.getAllCourses();
-        coursesTable.setItems(FXCollections.observableArrayList(courses));
-        courseID.setCellValueFactory(new PropertyValueFactory<>("CourseID"));
-        courseName.setCellValueFactory(new PropertyValueFactory<>("CourseName"));
-        courseyear.setCellValueFactory(new PropertyValueFactory<>("CourseDate"));
-        courseRoom.setCellValueFactory(new PropertyValueFactory<>("CourseRoom"));
-        courseDescriptionC.setCellValueFactory(new PropertyValueFactory<>("CourseDescription"));
-        startDate.setCellValueFactory(new PropertyValueFactory<>("startDate"));
-        endDate.setCellValueFactory(new PropertyValueFactory<>("endDate"));
-
-        return courseDao;
-    }
-
     @FXML
     public void backToTrainee(ActionEvent actionEvent) throws IOException {
         try {
@@ -123,6 +162,54 @@ public class ControllerCourseView implements Initializable {
         } catch (NullPointerException npe) {
             System.out.println(npe.getMessage());
         }
+    }
+
+    private void addUpdateButtonToTable(CourseDao courseDao) {
+        TableColumn<Course, Void> colBtn = new TableColumn("Update Course");
+
+        Callback<TableColumn<Course, Void>, TableCell<Course, Void>> cellFactory = new Callback<TableColumn<Course, Void>, TableCell<Course, Void>>() {
+            @Override
+            public TableCell<Course, Void> call(final TableColumn<Course, Void> param) {
+                final TableCell<Course, Void> cell = new TableCell<Course, Void>() {
+
+                    private final Button btn = new Button("Update");
+
+                    {
+                        btn.setOnAction((ActionEvent event) -> {
+
+                            try {
+                                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/CourseForm.fxml"));
+//                            loader = FXMLLoader.load(getClass().getResource("../view/TraineeForm.fxml"));
+                                Parent root = (Parent) fxmlLoader.load();
+
+                           /*     ControlleCourseForm controller = fxmlLoader.getController();
+                                controller.setCourse(coursesTable.getItems().get(getIndex()));
+                                controller.addNewCourse.setOnAction(event1 -> controller.updateCourse());
+                                btn.getScene().setRoot(root);*/
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+
+        colBtn.setCellFactory(cellFactory);
+
+        coursesTable.getColumns().add(colBtn);
     }
 
     @FXML
