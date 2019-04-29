f0ff102eccf28265214bd93077e6ed82928356bf
Ergn90 <g.michel90@gmail.com>
2019-04-29 16:30:55 +0200

    back after update + insert + remove warning css

--- TraineeProgramme/src/controller/ControlleTraineeForm.java
+++ TraineeProgramme/src/controller/ControlleTraineeForm.java
@@ -3,7 +3,9 @@ package controller;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.*;
 import model.location.Location;
 import model.location.LocationDao;
@@ -12,11 +14,11 @@ import model.trainee.Trainee;
 import model.trainee.TraineeDao;
 import model.trainee.TraineeDaoImpl;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -73,8 +75,8 @@ public class ControlleTraineeForm implements Initializable {
     private int currentTraineeID;
 
 
-    public void setTrainee(Trainee trainee){
-        currentTraineeID=trainee.getTraineeID();
+    public void setTrainee(Trainee trainee) {
+        currentTraineeID = trainee.getTraineeID();
         lastNameTrainee.setText(trainee.getLastName());
         firstNameTrainee.setText(trainee.getFirstName());
 
@@ -91,6 +93,7 @@ public class ControlleTraineeForm implements Initializable {
                 .atZone(ZoneId.systemDefault())
                 .toLocalDate();
     }
+
     public Date convertToDateViaInstant(LocalDate dateToConvert) {
         return java.util.Date.from(dateToConvert.atStartOfDay()
                 .atZone(ZoneId.systemDefault())
@@ -112,31 +115,32 @@ public class ControlleTraineeForm implements Initializable {
     @FXML
     public void addNewTrainee() {
 
-            TraineeDao trainee = new TraineeDaoImpl();
+        TraineeDao trainee = new TraineeDaoImpl();
 
-            Trainee tr = new Trainee();
-            LocationDao loc = new LocationDaoImpl();
+        Trainee tr = new Trainee();
+        LocationDao loc = new LocationDaoImpl();
 
-            tr.setLastName(lastNameTrainee.getText());
-            tr.setFirstName(firstNameTrainee.getText());
-            tr.setBirthday(convertToDateViaInstant(birthdayTrainee.getValue()));
-            tr.setAddress(addressTrainee.getText());
-            tr.setSchool(schoolTrainee.getText());
-            tr.setEmail(emailTrainee.getText());
-            tr.setLocation(loc.getLocation(locationTraineeCombo.getSelectionModel().getSelectedItem().getLocationId()));
+        tr.setLastName(lastNameTrainee.getText());
+        tr.setFirstName(firstNameTrainee.getText());
+        tr.setBirthday(convertToDateViaInstant(birthdayTrainee.getValue()));
+        tr.setAddress(addressTrainee.getText());
+        tr.setSchool(schoolTrainee.getText());
+        tr.setEmail(emailTrainee.getText());
+        tr.setLocation(loc.getLocation(locationTraineeCombo.getSelectionModel().getSelectedItem().getLocationId()));
 
-            trainee.insertTrainee(tr);
-            Platform.exit();
+        trainee.insertTrainee(tr);
+        backToTrainee();
 
-        }
+    }
+
     @FXML
-        public void cancelTrainee(){
-        Platform.exit();
-        }
+    public void cancelTrainee() {
+        backToTrainee();
+    }
 
     @FXML
     public void updateTrainee() {
-        
+
         TraineeDao updatedTrainee = new TraineeDaoImpl();
         Trainee tr = new Trainee();
 
@@ -147,14 +151,25 @@ public class ControlleTraineeForm implements Initializable {
         tr.setAddress(addressTrainee.getText());
         tr.setSchool(schoolTrainee.getText());
         tr.setEmail(emailTrainee.getText());
-        Location location=new Location();
+        Location location = new Location();
         location.setLocationId((locationTraineeCombo.getSelectionModel().getSelectedItem().getLocationId()));
         location.setLocationName((locationTraineeCombo.getSelectionModel().getSelectedItem().getLocationName()));
         tr.setLocation(location);
 
         updatedTrainee.updateTrainee(tr);
-        Platform.exit();
+        backToTrainee();
 
+    }
+
+    public void backToTrainee() {
+        try {
+            Parent loader = FXMLLoader.load(getClass().getResource("../view/TraineesView.fxml"));
+            addNewTrainee.getScene().setRoot(loader);
+        } catch (NullPointerException npe) {
+            System.out.println(npe.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
