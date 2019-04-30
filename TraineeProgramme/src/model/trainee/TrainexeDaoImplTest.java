96ece953d6738f0bbf183a022ea15339e09f90a7
andradaserban1 <andradaserban3636@gmail.com>
2019-04-30 16:39:41 +0200

    test

---
+++ TraineeProgramme/src/model/trainee/TrainexeDaoImplTest.java
@@ -0,0 +1,38 @@
+package model.trainee;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrainexeDaoImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getTrainee() {
+    }
+
+    @Test
+    public void getAllTrainee() {
+    }
+
+    @Test
+    public void insertTrainee() {
+    }
+
+    @Test
+    public void updateTrainee() {
+    }
+
+    @Test
+    public void deleteTrainee() {
+    }
+}
